4509407d3ebb445bbcb98a032b503235968c767b
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-02-23 23:34:54 +0000

    fetched user from given username for header

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,21 +2,22 @@ import { getUser } from "../data/api";
 import { useEffect, useState } from "react";
 
 const UserInfo = ({ username }) => {
-    // const [user, setUser] = useState(null);
+    const [currentUser, setCurrentUser] = useState(null);
     console.log("username", username);
 
-    // useEffect(() => {
-    //     console.log("fetching");
-    //     getUser(username).then((user) => {
-    //         console.log("fetched user", user);
-    //         setUser(user);
-    //     });
-    // }, []);
+    useEffect(() => {
+        console.log("fetching");
+        getUser(username).then((user) => {
+            console.log("fetched user", user);
+            setCurrentUser(user);
+            console.log(currentUser);
+        });
+    }, []);
 
     return (
         <div className="user-info">
             <p>{username}</p>
-            {/* <img src={user.avatar_url} />*/}
+            {currentUser && <img src={currentUser.avatar_url} />}
         </div>
     );
 };
