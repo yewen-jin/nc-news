df863207c78efc4fae43feeaf9f79203fce5f111
yewen-jin <yewen.elsa.jin@gmail.com>
2026-02-24 14:56:42 +0000

    updated published date&time to human readable format

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,5 +1,11 @@
 const ArticleCard = ({ article }) => {
-    console.log(article);
+    // console.log(article);
+    const publishedDate = article.created_at.split("T")[0].split("-");
+    const publishedTime = article.created_at.split("T")[1].split(".");
+    const date =
+        publishedDate[2] + "/" + publishedDate[1] + "/" + publishedDate[0];
+    const time = publishedTime[0].slice(0, -3);
+    console.log(publishedDate, publishedTime, date, time);
     return (
         <section className="article-card">
             <h3>{article.title}</h3>
@@ -10,7 +16,7 @@ const ArticleCard = ({ article }) => {
             />
             <p>{"Author:" + article.author}</p>
             <p>{"Topic:" + article.topic}</p>
-            <p>{"Published:" + article.created_at}</p>
+            <p>{"Published:" + date + " " + time}</p>
             <p>{"Votes:" + article.votes}</p>
             <p>{"Comments:" + article.comment_count}</p>
         </section>
