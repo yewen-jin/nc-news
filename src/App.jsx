6e443faf4476c9bbdc0fc5065926e527bd730745
yewen-jin <yewen.elsa.jin@gmail.com>
2026-02-24 13:45:31 +0000

    updated User file directory location

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./layout/Home";
 import Topics from "./layout/Topics";
+import User from "./layout/User";
 
 function App() {
     return (
@@ -12,7 +13,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/articles" element={<Home />} />
                 <Route path="/topics" element={<Topics />} />
-                <Route path="/users/:username" element={<User />} />
+                {/* <Route path="/users/:username" element={<User />} />*/}
             </Routes>
         </>
     );
