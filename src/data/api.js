e76501b799e35247e71b51052f84148c67cb105e
yewen-jin <yewen.elsa.jin@gmail.com>
2026-02-24 13:50:21 +0000

    added a list of fetch functions needed in data/api.js

--- src/data/api.js
+++ src/data/api.js
@@ -1,14 +1,9 @@
-export function getTopics() {
-    return;
-}
-
-export async function getUser(username) {
-    const url =
-        "https://nc-news-backend-91qy.onrender.com/api/users/" + username;
+export async function getTopics() {
+    const url = "https://nc-news-backend-91qy.onrender.com/api/topics";
     return fetch(url)
         .then((response) => {
             if (!response.ok) {
-                throw new Error(`HTTP error. Status: ${response.status}`);
+                throw new Error(`HTTP error, Status: ${response.status}`);
             }
             return response.json();
         })
@@ -17,23 +12,111 @@ export async function getUser(username) {
             return body;
         })
         .catch((error) => {
-            console.log("Fetch Error from: ", error.message);
+            console.log("Fetch Error from: ", url, error.message);
         });
 }
 
-export function getArticles() {
-    return;
+export async function getUser(username) {
+    const url =
+        "https://nc-news-backend-91qy.onrender.com/api/users/" + username;
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error, Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((body) => {
+            // console.log("fetched body:", body);
+            return body;
+        })
+        .catch((error) => {
+            console.log("Fetch Error from: ", url, error.message);
+        });
 }
 
-export function getArticleById() {
-    return;
+export async function getArticles() {
+    const url = "https://nc-news-backend-91qy.onrender.com/api/articles";
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error, Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((body) => {
+            // console.log("fetched body:", body);
+            return body;
+        })
+        .catch((error) => {
+            console.log("Fetch Error from: ", url, error.message);
+        });
 }
-export function getCommentsByArticle() {
-    return;
+
+export async function getArticleById(articleId) {
+    const url =
+        "https://nc-news-backend-91qy.onrender.com/api/articles/" + articleId;
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error, Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then((body) => {
+            // console.log("fetched body:", body);
+            return body;
+        })
+        .catch((error) => {
+            console.log("Fetch Error from: ", url, error.message);
+        });
 }
-export function upvote() {
-    return;
+
+export async function getCommentsByArticle(articleId) {
+    const url =
+        "https://nc-news-backend-91qy.onrender.com/api/articles/" +
+        articleId +
+        "/comments";
+    return fetch(url)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error(`HTTP error, Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .catch((error) => {
+            console.log("Fetch Error from: ", url, error.message);
+        });
 }
-export function downvote() {
-    return;
+
+export async function upvote(commentId) {
+    const url =
+        "https://nc-news-backend-91qy.onrender.com/api/comments/" + commentId;
+    return fetch(url, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ inc_votes: 1 }),
+    })
+        .then((response) => response.json())
+        .catch((error) => {
+            console.log("Fetch Error from: ", url, error.message);
+        });
+}
+
+export async function downvote(commentId) {
+    const url =
+        "https://nc-news-backend-91qy.onrender.com/api/comments/" + commentId;
+    return fetch(url, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ inc_votes: -1 }),
+    })
+        .then((response) => response.json())
+        .catch((error) => {
+            console.log("Fetch Error from: ", url, error.message);
+        });
 }
