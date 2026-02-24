8059b037539c17b901dc30949d53d122403b9b19
yewen-jin <yewen.elsa.jin@gmail.com>
2026-02-24 12:44:07 +0000

    added Link component to the nav bars

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,20 @@
+import { Link } from "react-router-dom";
+//current navbar setup only works if there is more than 1 nav items
+// otherwise the children element is not read as an array
+// fix this later.
 const NavBar = (props) => {
     // console.log(props);
     const links = props.children;
-    console.log("links: ", links);
+    const { destination } = props;
+    // console.log("links: ", links);
     return (
         <nav className="nav-bar">
-            {links.map((link) => {
-                return link;
+            {links.map((link, i) => {
+                return (
+                    <Link key={"nav-" + i} to={destination[i]}>
+                        {link.props.children}
+                    </Link>
+                );
             })}
         </nav>
     );
--- src/layout/Topics.jsx
+++ src/layout/Topics.jsx
@@ -1,12 +1,11 @@
-import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 const Topics = () => {
     return (
         <>
-            <NavBar>
-                <Link to="/topics">Topics</Link>
-                <Link to="/">Articles</Link>
+            <NavBar destination={["/", "/topics"]}>
+                <p>Articles</p>
+                <p>Topics</p>
             </NavBar>
         </>
     );
