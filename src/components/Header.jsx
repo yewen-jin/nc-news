a7c4bc05c2d75cba161cea460fc41432732f82b9
yewen-jin <yewen.elsa.jin@gmail.com>
2026-02-25 11:31:44 +0000

    used React Context to define the loggin user

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
-import { USERNAME } from "../data/user";
+// import { USERNAME } from "../data/user";
+import { useContext } from "react";
+import { UserContext } from "../context/User";
 import UserInfo from "./UserInfo";
 
 const Header = () => {
+    const username = useContext(UserContext);
+    console.log(username);
     return (
         <section className="header">
             <h1>NC NEWS </h1>
-            <UserInfo username={USERNAME} />
+            <UserInfo username={username} />
         </section>
     );
 };
