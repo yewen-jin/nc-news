4dc8910866085fdf88af55062cc1c07f46d4167c
yewen-jin <yewen.elsa.jin@gmail.com>
2026-02-25 10:52:21 +0000

    moved up/down votes buttons to Voting component

--- src/components/Interactions.jsx
+++ src/components/Interactions.jsx
@@ -1,13 +1,9 @@
 import Comments from "./Comments";
 import Voting from "./Voting";
-import upvote from "../assets/upvote.png";
-import downvote from "../assets/downvote.png";
 
 const Interactions = () => {
     return (
         <section className="article-interactions">
-            <img alt="downvote icon" src={downvote} />
-            <img alt="upvote icon" src={upvote} />
             <Voting />
             <Comments />
         </section>
--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -1,5 +1,12 @@
+import upvote from "../assets/upvote.png";
+import downvote from "../assets/downvote.png";
 const Voting = () => {
-    return <></>;
+    return (
+        <>
+            <img alt="downvote icon" src={downvote} />
+            <img alt="upvote icon" src={upvote} />
+        </>
+    );
 };
 
 export default Voting;
