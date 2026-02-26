3676883bdcfd9688e2d2129963e47260a537e11e
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-02-26 19:15:07 +0000

    use on change instead of button to change the sort by options

--- src/components/SortArticles.jsx
+++ src/components/SortArticles.jsx
@@ -1,45 +1,51 @@
 const SortArticles = ({ setSearchParams }) => {
-    const handleSubmit = (formData) => {
-        console.log(formData.get("sort_by"));
-        // setSearchParams((searchParams) => {
-        //     searchParams.set("sort_by", formData.value);
-        //     console.log(formData);
-        //     return searchParams;
-        // });
+    const handleChange = (e) => {
+        setSearchParams((searchParams) => {
+            searchParams.set("sort_by", e.target.value);
+            console.log("sort by: ", searchParams.get("sort_by"));
+            return searchParams;
+        });
     };
+
     return (
-        <>
-            <button
-                onClick={() => {
-                    setSearchParams((searchParams) => {
-                        searchParams.set("order", "asc");
-                        return searchParams;
-                    });
-                }}
-            >
-                Ascend
-            </button>
-
-            <button
-                onClick={() => {
-                    setSearchParams((searchParams) => {
-                        searchParams.set("order", "desc");
-                        return searchParams;
-                    });
-                }}
-            >
-                Descend
-            </button>
-
-            <form action={handleSubmit}>
-                <select id=" " name="sort_by" type="submit">
+        <div className="sort-articles">
+            <section className="sort-function">
+                <label htmlFor="sort_by">sort by:</label>
+                <select
+                    id="sort_by"
+                    name="sort_by"
+                    type="submit"
+                    defaultValue="created_at"
+                    onChange={handleChange}
+                >
                     <option value="created_at">date</option>
                     <option value="comment_count">comment count</option>
                     <option value="votes">votes</option>
                 </select>{" "}
-                <input type="submit"></input>
-            </form>
-        </>
+            </section>
+            <section className="sort-function">
+                <button
+                    onClick={() => {
+                        setSearchParams((searchParams) => {
+                            searchParams.set("order", "asc");
+                            return searchParams;
+                        });
+                    }}
+                >
+                    Ascend
+                </button>
+                <button
+                    onClick={() => {
+                        setSearchParams((searchParams) => {
+                            searchParams.set("order", "desc");
+                            return searchParams;
+                        });
+                    }}
+                >
+                    Descend
+                </button>
+            </section>
+        </div>
     );
 };
 
