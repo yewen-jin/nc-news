b2e3c9d0e4ef8e62a9a4213bf3ea2e3bccdeb7e9
Yewen Jin <yewen.elsa.jin@gmail.com>
2026-02-26 20:34:20 +0000

    created login component, added state for login status and change routing of home path depending on login status

--- src/layout/Home.jsx
+++ src/layout/Home.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react";
 import NavBar from "../components/NavBar";
 import Articles from "../components/Articles";
+import Login from "../components/Login";
 
 const Home = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     return (
         <>
             <NavBar destination={["/articles", "/topics"]}>
                 <p>Articles</p>
                 <p>Topics</p>
             </NavBar>
-            <Articles />
+            {!isLoggedIn && <Login />}
+            {isLoggedIn && <Articles />}
         </>
     );
 };
